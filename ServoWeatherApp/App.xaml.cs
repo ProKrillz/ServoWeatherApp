9b632cdeea16ab0f4df334aab5c43a29d764d471
ProKrillz <thomasdamkjaer@gmail.com>
2024-11-13 20:39:23 +0100

    Add project files.

---
+++ ServoWeatherApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ServoWeatherApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ServoWeatherApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ServoWeatherApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
