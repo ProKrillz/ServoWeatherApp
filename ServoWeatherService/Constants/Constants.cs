35f2156208bba46daa74c84f2b4513bb0cec71c5
ProKrillz <thomasdamkjaer@gmail.com>
2024-11-18 09:10:32 +0100

    blazor web app

--- ServoWeatherService/Constants/Constants.cs
+++ ServoWeatherService/Constants/Constants.cs
@@ -10,13 +10,13 @@ namespace ServoWeatherService.Constants
     {
         #region Brian Tunnel
 
-        public static string BaseUrl = "https://cb0q7p4s-7260.euw.devtunnels.ms"; // Brian
+        //public static string BaseUrl = "https://cb0q7p4s-7260.euw.devtunnels.ms"; // Brian
 
         #endregion
 
         #region Thomas Tunnel
 
-        //public static string BaseUrl = "https://xxx.euw.devtunnels.ms"; // Thomas
+        public static string BaseUrl = "https://4mcjs4n2-7260.euw.devtunnels.ms"; // Thomas
 
         #endregion
         public static string Endpoint = "GetTelemtry";
